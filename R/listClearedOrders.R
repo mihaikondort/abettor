191dcf2d52a9b59fe2cc555aa145b9a9f5bbb656
mihaikondort <mihai.kondort@yahoo.co.uk>
2017-07-01 13:43:45 +0100

    Update listClearedOrders.R

--- R/listClearedOrders.R
+++ R/listClearedOrders.R
@@ -150,7 +150,8 @@ listClearedOrders <-
     token <- Sys.getenv('token')
     
     headers <- list(
-      'Accept' = 'application/json', 'X-Application' = product, 'X-Authentication' = token, 'Content-Type' = 'application/json'
+      'Accept' = 'application/json', 'X-Application' = product, 'X-Authentication' = token, 'Content-Type' = 'application/json',
+      'Expect' = ''
     )
     
     listOrder <-
