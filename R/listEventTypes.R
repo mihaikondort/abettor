653ac1057285dc10cc104388b8c36bb385753c39
mihaikondort <mihai.kondort@yahoo.co.uk>
2017-07-01 13:45:09 +0100

    Update listEventTypes.R

--- R/listEventTypes.R
+++ R/listEventTypes.R
@@ -197,7 +197,8 @@ listEventTypes <-
 
 
     headers <- list(
-      'Accept' = 'application/json', 'X-Application' = product, 'X-Authentication' = token, 'Content-Type' = 'application/json'
+      'Accept' = 'application/json', 'X-Application' = product, 'X-Authentication' = token, 'Content-Type' = 'application/json',
+      'Expect' = ''
     )
 
     listEventTypes <-
